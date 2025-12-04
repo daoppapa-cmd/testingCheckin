e8e1468d2e933a68d87e1fae315c460d4b2be172
daoppapa-cmd <daoppapa@gmail.com>
2025-12-04 22:17:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1179,11 +1179,12 @@ function fetchEmployeesFromRTDB() {
         const group = (emp.group || "").trim();
         const dept = (emp.department || "").trim();
         
-        const isGroupMatch = group === "IT Support" || group === "DRB";
-        const isDeptMatch = dept === "training_ជំនាន់២";
+        //const isGroupMatch = group === "IT Support" || group === "DRB";
+        const isDeptMatch = dept === "Training_ជំនាន់២";
         
         // Use AND (&&) to include employees matching BOTH criteria
-        return isGroupMatch && isDeptMatch;
+        //return isGroupMatch && isDeptMatch;
+        return isDeptMatch;
     });
 
     renderEmployeeList(allEmployees);
